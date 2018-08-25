0f6cb67318db8b512e2bfb07d4dfc824a0682eae
Stepan <Agent_77oo7@mail.ru>
2018-08-25 20:11:26 +0300

    1

--- src/main/java/Massiv.java
+++ src/main/java/Massiv.java
@@ -1,6 +1,6 @@
 /**
  * Разработано 10.08.2018
- * Выбор работы с калькулятором или с массивом
+ * МАссив случайных чисел
  * @author Basov Stepan
  * @version 1.6
  */
@@ -8,13 +8,49 @@
 import Min.Min;
 
 
-
-public class Massiv {    public static void main(String[] args) {
-
+public class Massiv{    public static void main(String[] args) {
 
 
     Min New = new Min();
-    New.Minimalniy();
+    New.massivGeneration();
+
+    int f=0;
+    int z=0;
+    int a=16;
+
+    for (int j=0; j<New.getMas().length; j++)
+    {
+        New.setI(j);
+        //h запоминает элемент массива под интдексом j
+        int h= New.getIndex();
+       //Находим максимально отрицательный элемент массива, ноль не учитываем
+        if (h<f && h<z)
+        {
+            z=h;
+        }
+        //Находим минимальный положительный элемент массива, ноль не учитываем
+         if (h > f && h < a) {
+                a = h;
+            }
 
     }
+    System.out.println("Максимально отрицательный элемент массива = " + z);
+    System.out.println("Минимальный положительный элемент массива = " + a);
+
+    System.out.println("Новый массив:");
+//Меняем местами найденные нами элементы массива и выводим полученный массив
+    for (int l=0; l<New.getMas().length; l++) {
+        New.setI(l);
+        int h= New.getIndex();
+        if (h == a) {
+            h = z;
+        } else if (h == z) {
+            h=a;
+        }
+
+        System.out.println(l + " элемент массива = "+h);
+    }
 }
+
+}
+
--- src/main/java/Min/Min.java
+++ src/main/java/Min/Min.java
@@ -2,62 +2,26 @@ package Min;
 
 import java.util.Random;
 
-/**
- * Created by User on 20.08.2018.
- */
 public class Min {
-    public int i;
-    public int c;
-    public int m;
-    public  int a;
-    public int b;
-    public  int h;
-    public int k;
+    //определяем кол-во элементов в массиве, диапазон от -10 до 10 и 0 включительно, итого 21 символ
+    private int[] Mas = new int[21];
+    private int i;
 
-public void Minimalniy () {
-    Random random = new Random();
-//определяем кол-во элементов в массиве, диапазон от -10 до 10 и 0 включительно, итого 21 символ
-    int Mas[] = new int[21];
-//Заполняем массив случайных чисел и выводим на экран
-    for ( i = 0; i < Mas.length; i++) {
-        Mas[i] = random.nextInt(21) - 10;
-        System.out.println(i + " элемент массива = " + Mas[i]);
-    }
 
-     a = 0;
-     b = 0;
-//Находим максимально отрицательный элемент массива, ноль не учитываем
-    for (int j = 0; j < Mas.length; j++) {
-        if (Mas[j] < a && Mas[j] < b) {
-            b = Mas[j];
+    public void massivGeneration() {
+
+        //Заполняем массив случайных чисел в диапазоне -10 до 10 и 0 включительно и выводим на экран
+        Random random = new Random();
+        for (i = 0; i < Mas.length; i++) {
+            Mas[i] = random.nextInt(21) - 10;
+            System.out.println(i + " элемент массива = "+Mas[i]);
         }
     }
-    System.out.println("Максимально отрицательный элемент массива = " + b);
-    //Находим минимальный положительный элемент массива, ноль не учитываем
-    int c = 0;
-    int m = 16;
-    for (int h = 0; h < Mas.length; h++) {
-        if (Mas[h] > c && Mas[h] < m) {
-            m = Mas[h];
-        }
-    }
-    System.out.println("Минимальный положительный элемент массива = " + m);
-//Меняем местами найденные нами элементы массива и выводим полученный массив
 
-    for ( k=0; k<Mas.length; k++)
-    {
-        if (Mas[k]==b)
-        {
-            Mas[k]=m;
-        }
-        else if(Mas[k]==m)
-        {
-            Mas[k]=b;
-        }
-        System.out.println(k+" элемент массива теперь равен = "+Mas[k]);
-    }
+    public void setI(int i) {this.i = i;}
 
+    public int getIndex(){ return Mas[i];}
 
+    public int[] getMas() { return Mas;}
 }
 
-}
